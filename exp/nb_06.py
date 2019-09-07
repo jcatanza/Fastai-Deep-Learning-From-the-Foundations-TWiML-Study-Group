4bc5d913f2e709da26136f1e8b8f66620303a2f6
Joseph Catanzarite <jcatanza@users.noreply.github.com>
2019-09-07 03:46:42 -0700

    Added a few comments
    Signed-off-by: Joseph Catanzarite <jcatanza@users.noreply.github.com>

--- exp/nb_06.py
+++ exp/nb_06.py
@@ -49,19 +49,17 @@ class Hook():
 def append_stats(hook, mod, inp, outp):
     if not hasattr(hook,'stats'): hook.stats = ([],[])
     means,stds = hook.stats
-    if mod.training:
-        means.append(outp.data.mean())
-        stds .append(outp.data.std())
+    means.append(outp.data.mean())
+    stds .append(outp.data.std())
 
 class ListContainer():
     def __init__(self, items): self.items = listify(items)
     def __getitem__(self, idx):
-        try: return self.items[idx]
-        except TypeError:
-            if isinstance(idx[0],bool):
-                assert len(idx)==len(self) # bool mask
-                return [o for m,o in zip(idx,self.items) if m]
-            return [self.items[i] for i in idx]
+        if isinstance(idx, (int,slice)): return self.items[idx]
+        if isinstance(idx[0],bool):
+            assert len(idx)==len(self) # bool mask
+            return [o for m,o in zip(idx,self.items) if m]
+        return [self.items[i] for i in idx]
     def __len__(self): return len(self.items)
     def __iter__(self): return iter(self.items)
     def __setitem__(self, i, o): self.items[i] = o
--- exp/nb_07.py
+++ exp/nb_07.py
@@ -26,39 +26,3 @@ def conv_layer(ni, nf, ks=3, stride=2, bn=True, **kwargs):
               GeneralRelu(**kwargs)]
     if bn: layers.append(nn.BatchNorm2d(nf, eps=1e-5, momentum=0.1))
     return nn.Sequential(*layers)
-
-class RunningBatchNorm(nn.Module):
-    def __init__(self, nf, mom=0.1, eps=1e-5):
-        super().__init__()
-        self.mom, self.eps = mom, eps
-        self.mults = nn.Parameter(torch.ones (nf,1,1))
-        self.adds  = nn.Parameter(torch.zeros(nf,1,1))
-        self.register_buffer('sums', torch.zeros(1,nf,1,1))
-        self.register_buffer('sqrs', torch.zeros(1,nf,1,1))
-        self.register_buffer('count', tensor(0.))
-        self.register_buffer('factor', tensor(0.))
-        self.register_buffer('offset', tensor(0.))
-        self.batch = 0
-
-    def update_stats(self, x):
-        bs,nc,*_ = x.shape
-        self.sums.detach_()
-        self.sqrs.detach_()
-        dims = (0,2,3)
-        s    = x    .sum(dims, keepdim=True)
-        ss   = (x*x).sum(dims, keepdim=True)
-        c    = s.new_tensor(x.numel()/nc)
-        mom1 = s.new_tensor(1 - (1-self.mom)/math.sqrt(bs-1))
-        self.sums .lerp_(s , mom1)
-        self.sqrs .lerp_(ss, mom1)
-        self.count.lerp_(c , mom1)
-        self.batch += bs
-        means = self.sums/self.count
-        varns = (self.sqrs/self.count).sub_(means*means)
-        if bool(self.batch < 20): varns.clamp_min_(0.01)
-        self.factor = self.mults / (varns+self.eps).sqrt()
-        self.offset = self.adds - means*self.factor
-
-    def forward(self, x):
-        if self.training: self.update_stats(x)
-        return x*self.factor + self.offset
